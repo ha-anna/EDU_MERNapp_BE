41c108f85ced7f52b74d97e219184b66d836bf4d
Ha Anna <its.haanna@gmail.com>
2022-10-01 15:22:35 +0900

    Upload

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const express = require("express");
+import "dotenv/config.js";
+import express from "express";
+import mongoose from "mongoose";
 const app = express();
-const port = process.env.PORT || 3001;
+import { MONGO_URI_LOCAL } from './db/db.js'
+const PORT = process.env.PORT || 3001;
+const MONGO_URI = process.env.MONGO_URI
 
 app.get("/", (req, res) => {
-  console.log('Getting data')
+  console.log('Getting data', req, res)
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+mongoose.connect(MONGO_URI || MONGO_URI_LOCAL).then(() => {
+      app.listen(PORT, () => {
+      console.log('connected to db and listening on port 4000')
+        })
+    })
+    .catch((error) => {
+  console.log(error)
+})
