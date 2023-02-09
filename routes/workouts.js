ed4d38e383eb7f2bee2f2afff8637657ccb9e08f
Ha Anna <its.haanna@gmail.com>
2023-02-09 22:57:54 +0900

    Upload: user identification for workouts

---
+++ routes/workouts.js
@@ -0,0 +1,31 @@
+import express from "express";
+import {
+  getWorkouts, 
+  getWorkout, 
+  createWorkout, 
+  deleteWorkout, 
+  updateWorkout
+} from '../controllers/workoutController.js'
+import { requireAuth } from '../middleware/requireAuth.js'
+
+const router = express.Router()
+
+// require auth for all workout routes
+router.use(requireAuth)
+
+// GET all workouts
+router.get('/', getWorkouts)
+
+// GET a single workout
+router.get('/:id', getWorkout)
+
+// POST a new workout
+router.post('/', createWorkout)
+
+// DELETE a workout
+router.delete('/:id', deleteWorkout)
+
+// UPDATE a workout
+router.patch('/:id', updateWorkout)
+
+export { router }
