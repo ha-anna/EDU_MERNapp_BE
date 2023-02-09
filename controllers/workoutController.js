b654828c54b45210fa653269e8a9b8d43cdb2cfc
Ha Anna <83631167+its-haanna@users.noreply.github.com>
2023-02-09 23:51:31 +0900

    Update workoutController.js

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -3,7 +3,7 @@ import { Workout } from '../models/workoutModel.js'
 
 // get all workouts
 const getWorkouts = async (req, res) => {
-  const {user_id} = req.user._id
+  const user_id = req.user._id
   const workouts = await Workout.find({user_id}).sort({createdAt: -1})
 
   res.status(200).json(workouts)
